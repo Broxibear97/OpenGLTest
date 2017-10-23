7456315e38876d2219e023d7c3b559d670ff4b2a
Josh Hogarth <joshhogarth@live.co.uk>
2017-10-23 13:29:57 +0100

    Progress from second post of http://dreamstatecoding.blogspot.co.uk/2017/01/opengl-4-with-opentk-in-c-part-3.html

--- OpenGLTest/MainWindow.cs
+++ OpenGLTest/MainWindow.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL4;
@@ -11,6 +9,11 @@ namespace OpenGLTest
 {
     public sealed class MainWindow : GameWindow
     {
+        private int _program;
+        private int _vertexArray;
+
+
+
         public MainWindow() : base(1280, 720, GraphicsMode.Default, "OpenGL Test", GameWindowFlags.Default, DisplayDevice.Default, 4, 0, GraphicsContextFlags.ForwardCompatible)
         {
             Title += ": OpenGL Version: " + GL.GetString(StringName.Version);
@@ -24,6 +27,22 @@ namespace OpenGLTest
         protected override void OnLoad(EventArgs e)
         {
             CursorVisible = true;
+            _program = CompileShaders();
+            GL.GenVertexArrays(1, out _vertexArray);
+            GL.BindVertexArray(_vertexArray);
+            Closed += OnClosed;
+        }
+
+        private void OnClosed(object sender, EventArgs e)
+        {
+            Exit();
+        }
+
+        public override void Exit()
+        {
+            GL.DeleteVertexArrays(1, ref _vertexArray);
+            GL.DeleteProgram(_program);
+            base.Exit();
         }
 
         protected override void OnUpdateFrame(FrameEventArgs e)
@@ -49,7 +68,34 @@ namespace OpenGLTest
             GL.ClearColor(backColour);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
+            GL.UseProgram(_program);
+            GL.DrawArrays(PrimitiveType.Points, 0, 1);
+            GL.PointSize(10);
+
             SwapBuffers();
+        }
+
+        private int CompileShaders()
+        {
+            var vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShader, File.ReadAllText(@"Components\Shaders\vertexShader.vert"));
+            GL.CompileShader(vertexShader);
+
+            var fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShader, File.ReadAllText(@"Components\Shaders\fragmentShader.frag"));
+            GL.CompileShader(fragmentShader);
+
+            var program = GL.CreateProgram();
+            GL.AttachShader(program, vertexShader);
+            GL.AttachShader(program, fragmentShader);
+            GL.LinkProgram(program);
+
+            GL.DetachShader(program, vertexShader);
+            GL.DetachShader(program, fragmentShader);
+            GL.DeleteShader(vertexShader);
+            GL.DeleteShader(fragmentShader);
+
+            return program;
         }
 
     }
---
+++ OpenGLTest/Shaders/fragmentShader.frag
@@ -0,0 +1,8 @@
+﻿#version 440 core
+
+out vec4 colour;
+
+void main(void)
+{
+	colour = vec4(1.0, 0.0, 0.0, 1.0);
+}
